36522eca0d90318067d784df7a696699ea5315c3
Ashwin Narkar <ashwin.narkar@gmail.com>
2020-12-18 18:31:55 -0800

    Add if statement review exercises for Monday

---
+++ AshwinsFolder/Demos/ifStatements/ifStatementsReviewEx.cpp
@@ -0,0 +1,55 @@
+#include <stdlib.h>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+void exercise1()
+{
+	// This exercise will ask the user for 3 integers
+	// It will print the smallest of the number back
+
+	// Example Run:
+
+	// Please enter 3 numbers.
+	// 4
+	// 9
+	// 43
+	// The smallest number is 43.
+
+	// TODO: Add your code below
+	
+
+}
+
+void exercise2()
+{
+	// This will be an extension of exercise 1
+
+	// You will enter your scores from 4 exams.
+	// Your code should drop the lowest score and average the remaining 3 test scores
+
+	// Example Run:
+
+	// Please enter your 4 test scores
+	// 95
+	// 32
+	// 93
+	// 94
+	// Your average for the class is 94
+	
+	// TODO: your code below
+
+
+}
+
+int main()
+{
+	cout << "Exercise 1" << endl;
+	cout << "----------" << endl;
+	exercise1();
+	cout << endl;
+	cout << "Exercise 2" << endl;
+	cout << "----------" << endl;
+	exercise2();
+}
