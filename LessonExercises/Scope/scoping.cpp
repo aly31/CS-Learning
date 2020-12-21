2e6020aae2a1b8d4fdfce560c68c7a59a6b7bdb1
Ashwin Narkar <ashwin.narkar@gmail.com>
2020-12-20 16:29:21 -0800

    Variable scope exercise

---
+++ LessonExercises/Scope/scoping.cpp
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int questionsCorrect = 9;
+    int totalQuestions = 10;
+
+    if (questionsCorrect >= 9)
+    {
+        char letterGrade = 'A';
+    }
+
+    cout << letterGrade << endl;
+    
+}
