949287b63a6d445505c619bfaed2d33844a27454
Aaron Ly <aaronly@Aarons-MacBook-Pro.local>
2020-12-22 19:37:06 -0800

    correct spacing errors and added percentage

--- quizproj/quizproj.cpp
+++ quizproj/quizproj.cpp
@@ -9,141 +9,142 @@ int main()
 {
 	int x;
 	double total;
-	cout<<"This is a math quiz. It consists of ten questions."<<endl;
+	cout <<"This is a math quiz. It consists of ten questions." <<endl;
 	
-	cout<<"1. What is 2 + 2?"<<endl;
+	cout <<"1. What is 2 + 2?" <<endl;
 	cin >> x;
 	if (x != 4)
 	{
-		cout<<"That is incorrect!"<<endl;
+		cout <<"That is incorrect!" <<endl;
 		total = 0;	
 	}
 	else
 	{
-		cout<<"That is correct!"<<endl;
+		cout <<"That is correct!" <<endl;
 		total = 1;		
 	}	
 
-	cout<<"2. What is 2 - 2?"<<endl;
+	cout <<"2. What is 2 - 2?" <<endl;
 	cin >> x;
 	if (x != 0)
 	{
-		cout<<"That is incorrect!"<<endl;
+		cout <<"That is incorrect!" <<endl;
 		total = 0;
 	}
 	else
 	{
-		cout<<"That is correct!"<<endl;
+		cout <<"That is correct!" <<endl;
 		total = total + 1;	
 	}
 
-	cout<<"3. What is 3 + 4?"<<endl;
+	cout <<"3. What is 3 + 4?" <<endl;
 	cin >> x;
 	if (x != 7)
 	{
-		cout<<"That is incorrect!"<<endl;
+		cout <<"That is incorrect!" <<endl;
 		total = 0;
 	}	
 	else
 	{
-		cout<<"That is correct!"<<endl;
+		cout <<"That is correct!" <<endl;
 		total = total + 1;
 	}
 
-	cout<<"4. What is 9 - 5?"<<endl;
+	cout <<"4. What is 9 - 5?" <<endl;
 	cin >> x;
 	if (x != 4)
 	{
-		cout<<"That is incorrect!"<<endl;
+		cout <<"That is incorrect!" <<endl;
 		total = 0;
 	}
 	else
 	{
-		
-		cout<<"That is correct!"<<endl;
+		cout <<"That is correct!" <<endl;
 		total = total + 1;		
 	}	
 	
-	cout<<"5. What is 5 * 9?"<<endl;
+	cout <<"5. What is 5 * 9?" <<endl;
 	cin >> x;
 	if (x !=45)
 	{
-		cout<<"That is incorrect!"<<endl;
+		cout <<"That is incorrect!" <<endl;
 		total = 0;
 	}
 	else
 	{
-		cout<<"That is correct!"<<endl;
+		cout <<"That is correct!" <<endl;
 		total = total + 1;		
 	}
 	
-	cout<<"6. What is 4 * 7?"<<endl;	
+	cout <<"6. What is 4 * 7?" <<endl;	
 	cin >> x;
 	if (x != 28)
 	{
-		cout<<"That is incorrect!"<<endl;
+		cout <<"That is incorrect!" <<endl;
 		total = 0;
 	}
 	else
 	{
-		cout<<"That is correct!"<<endl;
+		cout <<"That is correct!" <<endl;
 		total= total + 1;		
 	}	
 	
-	cout<<"7. What is the remainder of 9/3?"<<endl;
+	cout <<"7. What is the remainder of 9/3?" <<endl;
 	cin >> x;
 	if(x != 0)
 	{
-		cout<<"That is incorrect!"<<endl;
+		cout <<"That is incorrect!" <<endl;
 		total = 0;
 	}
 	else
 	{
-		cout<<"That is correct!"<<endl;
+		cout <<"That is correct!" <<endl;
 		total = total + 1;
 	}
 
-	cout<<"8. What is 10/2?"<<endl;
+	cout <<"8. What is 10/2?" <<endl;
 	cin>> x;
 	if(x != 5)
 	{
-		cout<<"That is incorrect!"<<endl;
+		cout <<"That is incorrect!" <<endl;
 		total = 0;
 	}
 	else
 	{
-		cout<<"That is correct!"<<endl;
+		cout <<"That is correct!" <<endl;
 		total = total + 1;	
 	}
 
-	cout<<"9. What is (9 + 5) * 3?"<<endl;
+	cout <<"9. What is (9 + 5) * 3?" <<endl;
 	cin>> x;
 	if (x != 42)
 	{
-		cout<<"That is incorrect!"<<endl;
+		cout <<"That is incorrect!" <<endl;
 		total = 0;
 	}
 	else
 	{
-		cout<<"That is correct!"<<endl;
+		cout <<"That is correct!" <<endl;
 		total = total + 1;
 	}
 
-	cout<<"10. What is answer from question 2 * answer  from question 9?"<<endl;
+	cout <<"10. What is answer from question 2 * answer  from question 9?" <<endl;
 	cin>> x;
 	if(x != 0)
 	{
-		cout<<"That is incorrect!"<<endl;
+		cout <<"That is incorrect!" <<endl;
 		total = 0;
 	}	
 	else
 	{	
-		cout<<"That is correct!"<<endl;
+		cout <<"That is correct!" <<endl;
 		total = total + 1;
 	}
-	
+	double percentage;
+	percentage = (total/10) * 100;
 
-	cout<<"You got " << total << " out of 10 questions correct" << "." <<endl;
+
+	cout <<"You got " << total << " out of 10 questions correct" << "." << "That's a " << percentage << "%!" <<endl;
 
 }
 
