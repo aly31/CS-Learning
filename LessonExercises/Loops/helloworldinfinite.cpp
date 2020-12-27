dc286e2afb3a022fa0b79cd6db771710eed8b818
Aaron Ly <aaronly@Aarons-MacBook-Pro.local>
2020-12-27 13:09:43 -0800

    Finished helloworldupgradewithAsh

---
+++ LessonExercises/Loops/helloworldinfinite.cpp
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	int a;
+	cin >> a;
+	int i = a;
+	while (i >= 1)
+	{
+		cout  << i << endl;
+		i--; 
+	}
+	cout << "The value of i is " << i << "." << endl;
+}
