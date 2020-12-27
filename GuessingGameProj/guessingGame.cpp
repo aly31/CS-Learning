5f6016db2976baf5644cc8ce1bf5ad2855cfc7d4
Ashwin Narkar <ashwin.narkar@gmail.com>
2020-12-27 00:44:44 -0800

    Added Guessing Game Project Spec

---
+++ GuessingGameProj/guessingGame.cpp
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// Your program will create a random number from 1 to 100 (including 1 and 100).
+	// You will ask the user to guess this random number. 
+	// If the user guesses the wrong number,
+	//		You should print out, "Your answer was too big/too small! Try again!"
+	// When they get the correct answer,
+	// Print out "You guessed correctly! It only took you (the number of tries it took them) tries!"
+
+
+	// How to generate a random number:
+	// rand() is a function that generates a random integer from 0 to RANDMAX
+	// 		RANDMAX is a variable defined in <stdlib.h> (from line 1). 
+	//		RANDMAX is computer dependent but is guaranteed to be at least 32767.
+
+	// If I want to get a random number n in the range x <= n < y
+	//		int n = (rand() % (y-x)) + x
+
+	// GOOD LUCK!
+
+
+
+
+}
