2f054b0e1098477a04e48e358a11105b7f22610f
Aaron Ly <aaronly@Aarons-MacBook-Pro.local>
2020-12-30 10:03:59 -0800

    added a variable for number of questions. fixed question 1 and 2

--- quizproj/quizproj.cpp
+++ quizproj/quizproj.cpp
@@ -9,151 +9,154 @@ int main()
 {
 	int x;
 	double total;
+
+	int questions;
+	questions = 1;
+	cout << "This is a math quiz. It consists of ten questions." <<endl;
 	
-	cout <<"This is a math quiz. It consists of ten questions." <<endl;
-	
-	cout <<"1. What is 2 + 2?" <<endl;
+	cout << questions << " What is 2 + 2?" <<endl;
 	cin >> x;
 	if (x != 4)
 	{
-		cout <<"That is incorrect!" <<endl;	
+		cout << "That is incorrect!" <<endl;	
 	}
 	else
 	{
-		cout <<"That is correct!" <<endl;
+		cout << "That is correct!" <<endl;
 		total = 1;		
 	}	
 
-	cout <<"2. What is 2 - 2?" <<endl;
+	cout << questions + 1 << " What is 2 - 2?" <<endl;
 	cin >> x;
 	if (x != 0)
 	{
-		cout <<"That is incorrect!" <<endl;
+		cout << "That is incorrect!" <<endl;
 	}
 	else
 	{
-		cout <<"That is correct!" <<endl;
+		cout << "That is correct!" <<endl;
 		total = total + 1;	
 	}
 
-	cout <<"3. What is 3 + 4?" <<endl;
+	cout << "3. What is 3 + 4?" <<endl;
 	cin >> x;
 	if (x != 7)
 	{
-		cout <<"That is incorrect!" <<endl;
+		cout << "That is incorrect!" <<endl;
 	}	
 	else
 	{
-		cout <<"That is correct!" <<endl;
+		cout << "That is correct!" <<endl;
 		total = total + 1;
 	}
 
-	cout <<"4. What is 9 - 5?" <<endl;
+	cout << "4. What is 9 - 5?" <<endl;
 	cin >> x;
 	if (x != 4)
 	{
-		cout <<"That is incorrect!" <<endl;
+		cout << "That is incorrect!" <<endl;
 	}
 	else
 	{
-		cout <<"That is correct!" <<endl;
+		cout << "That is correct!" <<endl;
 		total = total + 1;		
 	}	
 	
-	cout <<"5. What is 5 * 9?" <<endl;
+	cout << "5. What is 5 * 9?" <<endl;
 	cin >> x;
 	if (x !=45)
 	{
-		cout <<"That is incorrect!" <<endl;
+		cout << "That is incorrect!" <<endl;
 	}
 	else
 	{
-		cout <<"That is correct!" <<endl;
+		cout << "That is correct!" <<endl;
 		total = total + 1;		
 	}
 	
-	cout <<"6. What is 4 * 7?" <<endl;	
+	cout << "6. What is 4 * 7?" <<endl;	
 	cin >> x;
 	if (x != 28)
 	{
-		cout <<"That is incorrect!" <<endl;
+		cout << "That is incorrect!" <<endl;
 	}
 	else
 	{
-		cout <<"That is correct!" <<endl;
+		cout << "That is correct!" <<endl;
 		total= total + 1;		
 	}	
 	
-	cout <<"7. What is the remainder of 9/3?" <<endl;
+	cout << "7. What is the remainder of 9/3?" <<endl;
 	cin >> x;
 	if(x != 0)
 	{
-		cout <<"That is incorrect!" <<endl;
+		cout << "That is incorrect!" <<endl;
 	}
 	else
 	{
-		cout <<"That is correct!" <<endl;
+		cout << "That is correct!" <<endl;
 		total = total + 1;
 	}
 
-	cout <<"8. What is 10/2?" <<endl;
+	cout << "8. What is 10/2?" <<endl;
 	cin>> x;
 	if(x != 5)
 	{
-		cout <<"That is incorrect!" <<endl;
+		cout << "That is incorrect!" <<endl;
 	}
 	else
 	{
-		cout <<"That is correct!" <<endl;
+		cout << "That is correct!" <<endl;
 		total = total + 1;	
 	}
 
-	cout <<"9. What is (9 + 5) * 3?" <<endl;
+	cout << "9. What is (9 + 5) * 3?" <<endl;
 	cin>> x;
 	if (x != 42)
 	{
-		cout <<"That is incorrect!" <<endl;
+		cout << "That is incorrect!" <<endl;
 	}
 	else
 	{
-		cout <<"That is correct!" <<endl;
+		cout << "That is correct!" <<endl;
 		total = total + 1;
 	}
 
-	cout <<"10. What is answer from question 2 * answer  from question 9?" <<endl;
+	cout << "10. What is answer from question 2 * answer  from question 9?" <<endl;
 	cin>> x;
 	if(x != 0)
 	{
-		cout <<"That is incorrect!" <<endl;
+		cout << "That is incorrect!" <<endl;
 	}	
 	else
 	{	
-		cout <<"That is correct!" <<endl;
+		cout << "That is correct!" <<endl;
 		total = total + 1;
 	}
-	double percentage;
-	percentage = (total / 10) * 100;
 	
-	cout <<"You got " << total << " out of 10 questions correct" << "." << "That's a " << percentage << "%!" <<endl;
+	double percentage;
+	percentage = (total / questions) * 100;
+	
+	cout << "You got " << total << " out of 10 questions correct" << "." << "That's a " << percentage << "%!" <<endl;
 	if (percentage >= 90 && percentage <= 100)
 	{
-		cout <<"A " << percentage << " is an A" << "." << endl;
+		cout << "A " << percentage << " is an A" << "." << endl;
 	}
 	if (percentage >= 80 && percentage <= 89)
 	{
-		cout <<"A " << percentage << " is a B" << "." << endl;
+		cout << "A " << percentage << " is a B" << "." << endl;
 	}
 	if (percentage >= 70 && percentage <= 79)
 	{
-		cout <<"A " << percentage << " is a C" << "." << endl;
+		cout << "A " << percentage << " is a C" << "." << endl;
 	}
 	if (percentage >= 60 && percentage <= 69)
 	{
-		cout <<"A " << percentage << " is a D" << "." << endl;
+		cout << "A " << percentage << " is a D" << "." << endl;
 	}
 	if (percentage <= 59)
 	{
-		cout <<"A " << percentage << " is a F" << "." << endl;
+		cout << "A " << percentage << " is a F" << "." << endl;
 	}
 	
 
