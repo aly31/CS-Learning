61fd023e3c8856dc45a1fff26e86e3fc794deb7a
Ashwin Narkar <ashwin.narkar@gmail.com>
2021-01-08 20:08:40 -0800

    Special Characters Demos

---
+++ AshwinsFolder/Demos/specialcharacters.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    cout << "How would I print out a quotation mark?" << endl;
+    cout << "\"" << endl;       // Using the backslash lets me print the quotation mark
+    cout << "Hi\nHello\n";   // This prints on 2 different lines
+    
+}
